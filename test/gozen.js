9b30538fc2dadbdebc4a065dd82ba7508fcb3df1
webmech <webmech@gmail.com>
2014-09-08 23:07:44 -0400

    fixed a few issues with missing parent meothods

--- test/gozen.js
+++ test/gozen.js
@@ -5,6 +5,31 @@ var expect = require('chai').expect;
 var Class = require('../lib/class');
 
 describe('Class', function() {
+  it('Should call its parent contstructor when it has none', function() {
+    var Foo = new Class({
+      init: function() {
+        this.name = 'foo';
+      }
+    });
+
+    var Bar = new Foo({
+      extend: true,
+      init: function() {
+        this._super.init.call(this);
+        this.age = 50;
+      }
+    });
+
+    var Baz = new Bar({
+      extend: true
+    });
+
+    var baz = new Baz();
+
+    expect(baz).to.have.property('name');
+    expect(baz).to.have.property('age');
+  });
+
   it('Should should initialize', function() {
     var Person = new Class({
       extend: true,
@@ -14,13 +39,13 @@ describe('Class', function() {
     });
     var Mike = new Person({
       extend: true,
-      cool: 'yes',
+      cool: 'Y',
       init: function __construct_mike(options) {
         this._super.init.call(this, options || {});
-        this.name = this.name +' '+'mike';
+        this.name = this.name + ' ' + 'mike';
       },
-      bar : function(){
-        console.log('mike ', 'bar');
+      bar: function() {
+        //console.log('mike ', 'bar');
       }
     });
 
@@ -28,16 +53,19 @@ describe('Class', function() {
       extend: true,
       init: function __construct_mav(options) {
         this._super.init.call(this, options || {});
-        this.name = this.name+' '+options.name;
+        this.name = this.name + ' ' + options.name;
         //this.name = this.name + ' '+ options.name;
       },
-      bar: function(){
+      bar: function() {
         this._super.bar();
       }
     });
 
-    var mav = new Mav({name: 'mav'});
+    var mav = new Mav({
+      name: 'mav'
+    });
     expect(mav.name).to.eql('person mike mav');
+    expect(mav).to.have.property('cool');
     //expect(Mav.prototype._super).to.eql(Mike.prototype);
   });
 });
